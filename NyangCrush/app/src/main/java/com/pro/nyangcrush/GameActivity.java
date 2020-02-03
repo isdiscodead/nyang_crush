2fdf80cfdea74f2832324e9a2f6a55628bd07105
isdiscodead <hyhy1274@naver.com>
2020-02-03 13:09:45 +0900

    [ 지원 ] 다이얼로그 적용 및 수정

--- NyangCrush/app/src/main/java/com/pro/nyangcrush/GameActivity.java
+++ NyangCrush/app/src/main/java/com/pro/nyangcrush/GameActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import com.pro.nyangcrush.databinding.ActivityGameBinding;
 import com.pro.nyangcrush.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class GameActivity extends Activity {
 
     ActivityGameBinding binding;
+    Dialog dialog;
+
+    // 일시정지 다이얼로그 내부의 버튼들
+    Button btn_replay, btn_stop, btn_back, btn_close ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,47 @@ public class GameActivity extends Activity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_game);
         binding.setActivity(this);
 
+        /* pause 버튼 눌렀을 때 다이얼로그 생성 */
+        binding.btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog( GameActivity.this );
 
+                dialog.setCancelable( false );
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.setContentView( R.layout.diag_pause );
+
+                btn_back = dialog.findViewById( R.id.btn_back );
+                btn_stop = dialog.findViewById( R.id.btn_stop );
+                btn_replay = dialog.findViewById( R.id.btn_replay );
+                btn_close = dialog.findViewById( R.id.btn_close );
+
+                dialog.show();
+
+                btn_back.setOnClickListener( dialClick );
+                btn_stop.setOnClickListener( dialClick );
+                btn_replay.setOnClickListener( dialClick );
+                btn_close.setOnClickListener( dialClick );
+            }
+        });
     }
+
+    // 다이어그램 온클릭 리스너
+    View.OnClickListener dialClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch ( view.getId() ){
+                case R.id.btn_replay :
+                    break;
+                case R.id.btn_stop :
+                    break;
+                default:
+                    dialog.dismiss();
+                    break;
+
+            }
+        }
+    };
 }
--- NyangCrush/app/src/main/java/com/pro/nyangcrush/MainActivity.java
+++ NyangCrush/app/src/main/java/com/pro/nyangcrush/MainActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.pro.nyangcrush.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 public class MainActivity extends Activity {
 
      ActivityMainBinding binding;
+     Dialog dialog;
+
+     // 다이얼로그 내부 버튼
+    Button btn_close, btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,76 @@ public class MainActivity extends Activity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
 
+        /* 도움말 버튼 눌렀을 때 다이얼로그 생성 */
+        binding.btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog( MainActivity.this );
 
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.setContentView( R.layout.diag_howtp );
+
+                btn_close = dialog.findViewById(R.id.btn_close);
+                btn_close.setOnClickListener( dialClick );
+
+                dialog.show();
+            }
+        });
+
+        /* 설정 버튼 눌렀을 때 다이얼로그 생성 */
+        binding.btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog( MainActivity.this );
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.setContentView( R.layout.diag_setting );
+
+                btn_close = dialog.findViewById(R.id.btn_close);
+                btn_logout = dialog.findViewById( R.id.btn_logout );
+                btn_close.setOnClickListener( dialClick );
+                btn_logout.setOnClickListener( dialClick );
+
+                dialog.show();
+            }
+        });
+
+        /* 랭킹 버튼 눌렀을 때 다이얼로그 생성 */
+        binding.btnRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog( MainActivity.this );
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.setContentView( R.layout.diag_rank );
+
+                btn_close = dialog.findViewById(R.id.btn_close);
+                btn_close.setOnClickListener( dialClick );
+
+                dialog.show();
+            }
+        });
     }
+
+    // 다이어그램 온클릭 리스너
+    View.OnClickListener dialClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch ( view.getId() ){
+                case R.id.btn_close :
+                    dialog.dismiss();
+                    break;
+                case R.id.btn_logout :
+                    Toast.makeText(MainActivity.this, "로그아웃", Toast.LENGTH_SHORT).show();
+                    break;
+
+            }
+        }
+    };
 }
