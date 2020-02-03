9db9f3fdf5b11496a318dac48a7773c3f3c7e73c
lyj_95 <dldudwnyyy95@gmail.com>
2020-02-03 18:40:57 +0900

    [영주] login.xml이랑 loginActivity 만들었어요

--- NyangCrush/app/src/main/java/com/pro/nyangcrush/LoginActivity.java
+++ NyangCrush/app/src/main/java/com/pro/nyangcrush/LoginActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.pro.nyangcrush.databinding.ActivityLoginBinding;
 import com.pro.nyangcrush.databinding.ActivityMainBinding;
@@ -13,12 +16,39 @@ public class LoginActivity extends Activity {
 
     ActivityLoginBinding binding;
 
+    Button btn_logIn, btn_join;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         binding.setActivity(this);
 
+        btn_logIn = binding.btnLogIn;
+        btn_join = binding.btnJoin;
 
-    }
+        btn_logIn.setOnClickListener( click );
+
+    } // onCreate()
+
+    View.OnClickListener click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+
+                case R.id.btn_logIn:
+                    /* 임시로 */
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    break;
+
+                case R.id.btn_join:
+                    break;
+
+            } // switch
+
+        }
+    };
+
 }
