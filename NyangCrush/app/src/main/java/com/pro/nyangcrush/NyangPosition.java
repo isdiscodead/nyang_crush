ca0aa758d7875ba49b5f18339832de80ea814c22
lyj_95 <dldudwnyyy95@gmail.com>
2020-02-03 16:43:40 +0900

    [영주] imageview, rounded.xml추가

---
+++ NyangCrush/app/src/main/java/com/pro/nyangcrush/NyangPosition.java
@@ -0,0 +1,21 @@
+package com.pro.nyangcrush;
+
+public class NyangPosition {
+
+    private int x;
+    private int y;
+
+    public NyangPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+} // NyangPosition()
